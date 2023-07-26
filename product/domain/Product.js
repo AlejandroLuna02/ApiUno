37092fc51c7708286dddca7d69f5a2c6cc583017
AlejandroLuna02 <chusitoluna2002@gmail.com>
2023-07-26 04:24:30 -0600

    first commit

---
+++ product/domain/Product.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    constructor(id, humedad) {
+        this.id = id;
+        this.humedad = humedad;
+    }
+}
+exports.Product = Product;
